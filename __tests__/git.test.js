615a093a953c071886f2e1a1a2fc3de4ee278112
Geoff Selby <geoff.w.selby@gmail.com>
2019-01-13 18:06:31 -0600

    Add Git module

---
+++ __tests__/git.test.js
@@ -0,0 +1,51 @@
+import moxios from 'moxios';
+import Forge from '../src/Forge';
+
+beforeEach(() => {
+  moxios.install();
+});
+
+afterEach(() => {
+  moxios.uninstall();
+});
+
+test('it installs a new repository', async () => {
+  moxios.stubRequest('/servers/1/sites/1/git', {
+    status: 200,
+  });
+
+  const forge = new Forge('API_TOKEN');
+  const repo = await forge.git.install(1, 1, {
+    provider: 'github',
+    repository: 'username/respository',
+    branch: 'master',
+  });
+
+  expect(repo.status).toEqual(200);
+});
+
+test('it updates a repository', async () => {
+  moxios.stubRequest('/servers/1/sites/1/git', {
+    status: 200,
+  });
+
+  const forge = new Forge('API_TOKEN');
+  const repo = await forge.git.update(1, 1, {
+    provider: 'github',
+    repository: 'username/respository',
+    branch: 'master',
+  });
+
+  expect(repo.status).toEqual(200);
+});
+
+test('it removes a repository', async () => {
+  moxios.stubRequest('/servers/1/sites/1/git', {
+    status: 200,
+  });
+
+  const forge = new Forge('API_TOKEN');
+  const repo = await forge.git.remove(1, 1);
+
+  expect(repo.status).toEqual(200);
+});
