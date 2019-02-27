b1ea362307a29ddee927d4316246b54cd742fedb
Geoff Selby <geoff.w.selby@gmail.com>
2019-02-26 23:57:34 -0600

    Add 'Accept' and 'Content-Type' Headers to all requests

--- src/core/ForgeRequest.js
+++ src/core/ForgeRequest.js
@@ -7,6 +7,8 @@ class ForgeRequest {
       baseUrl: 'https://forge.laravel.com/api/v1',
       headers: {
         'Authorization': `Bearer ${token}`,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
       },
     });
   }
